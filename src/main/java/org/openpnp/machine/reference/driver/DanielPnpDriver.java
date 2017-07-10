8f273810fbbbd6a5f294464de797d8ed2f19dbcc
Alexander Rössler <mail@roessler.systems>
2017-07-10 21:02:21 +0200

    DanielPnpDriver: fix styling problems

--- src/main/java/org/openpnp/machine/reference/driver/DanielPnpDriver.java
+++ src/main/java/org/openpnp/machine/reference/driver/DanielPnpDriver.java
@@ -53,18 +53,18 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
     private Location homeLocation = new Location(LengthUnit.Millimeters);
 
     private int placeBlowTime = 50;
-    
+
     private static int X_AXIS = 2;
     private static int Y_AXIS = 1;
     private static int Z_AXIS = 3;
     private static int C_AXIS = 4;
-    
+
     private double[] stepsPerMm;
     private double[] minimumLimits;
     private double[] maximumLimits;
     private double[] feedMmPerMinute;
     private double[] accelMmPerSecondSquared;
-    private double cAxisCorrectionRadius = 0;//0.25;
+    private double cAxisCorrectionRadius = 0;// 0.25;
 
     private double x, y, z, c;
     private double xCorrection, yCorrection;
@@ -73,87 +73,88 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
     private Object commandLock = new Object();
     private String lastResponse;
     private boolean connected;
-    
+
     private DigitalOutputs digitalOutputsBank1 = new DigitalOutputs(1);
     private DigitalOutputs digitalOutputsBank2 = new DigitalOutputs(2);
 
     public DanielPnpDriver() {
-    	initializeStepsPerRevolution();
-    	initializeLimits();
-    	initializeFeedRate();
-    	initializeAcceleration();
-    }    
-    
+        initializeStepsPerRevolution();
+        initializeLimits();
+        initializeFeedRate();
+        initializeAcceleration();
+    }
+
     private void initializeStepsPerRevolution() {
-    	stepsPerMm = new double[5];
-    	stepsPerMm[X_AXIS] = 160.08; // was 160
-    	stepsPerMm[Y_AXIS] = -160.08; // was 160
-    	stepsPerMm[Z_AXIS] = -80.0;
-    	stepsPerMm[C_AXIS] = -320.0 / 18.0;
+        stepsPerMm = new double[5];
+        stepsPerMm[X_AXIS] = 160.08; // was 160
+        stepsPerMm[Y_AXIS] = -160.08; // was 160
+        stepsPerMm[Z_AXIS] = -80.0;
+        stepsPerMm[C_AXIS] = -320.0 / 18.0;
     }
-    
+
     private void initializeLimits() {
-    	minimumLimits = new double[5];
-    	maximumLimits = new double[5];
-    	minimumLimits[X_AXIS] = 0.0;
-    	maximumLimits[X_AXIS] = 600.0;
-    	minimumLimits[Y_AXIS] = -685.0;
-    	maximumLimits[Y_AXIS] = 0.0;
-    	minimumLimits[Z_AXIS] = -80.0;
-    	maximumLimits[Z_AXIS] = 0.0;
+        minimumLimits = new double[5];
+        maximumLimits = new double[5];
+        minimumLimits[X_AXIS] = 0.0;
+        maximumLimits[X_AXIS] = 600.0;
+        minimumLimits[Y_AXIS] = -685.0;
+        maximumLimits[Y_AXIS] = 0.0;
+        minimumLimits[Z_AXIS] = -80.0;
+        maximumLimits[Z_AXIS] = 0.0;
     }
-    
+
     private void initializeFeedRate() {
-    	feedMmPerMinute = new double[5];
-    	feedMmPerMinute[X_AXIS] = 40000.0;
-    	feedMmPerMinute[Y_AXIS] = 30000.0;
-    	feedMmPerMinute[Z_AXIS] = 80000.0;
-    	feedMmPerMinute[C_AXIS] = 100000.0;
+        feedMmPerMinute = new double[5];
+        feedMmPerMinute[X_AXIS] = 40000.0;
+        feedMmPerMinute[Y_AXIS] = 30000.0;
+        feedMmPerMinute[Z_AXIS] = 80000.0;
+        feedMmPerMinute[C_AXIS] = 100000.0;
     }
-    
+
     private void initializeAcceleration() {
-    	accelMmPerSecondSquared = new double[5];
-    	accelMmPerSecondSquared[X_AXIS] = 200000.0;
-    	accelMmPerSecondSquared[Y_AXIS] = 160000.0;
-    	accelMmPerSecondSquared[Z_AXIS] = 300000.0;
-    	accelMmPerSecondSquared[C_AXIS] = 400000.0;
+        accelMmPerSecondSquared = new double[5];
+        accelMmPerSecondSquared[X_AXIS] = 200000.0;
+        accelMmPerSecondSquared[Y_AXIS] = 160000.0;
+        accelMmPerSecondSquared[Z_AXIS] = 300000.0;
+        accelMmPerSecondSquared[C_AXIS] = 400000.0;
     }
-    
+
     private boolean checkLimit(int axis, double pos) {
-    	return ((pos >= minimumLimits[axis]) && (pos <= maximumLimits[axis]));
+        return ((pos >= minimumLimits[axis]) && (pos <= maximumLimits[axis]));
     }
 
     private class DriverStatus {
         public boolean ready = false;
         public boolean zeroPos = false;
         public boolean posError = false;
-        
-        public DriverStatus() { }
+
+        public DriverStatus() {}
     }
-    
+
     private class DigitalOutputs {
-    	public boolean suck = false;
-    	public boolean blow = false;
-    	public boolean light = false;
-    	public int bank = 0;
-    	
-    	public DigitalOutputs(int bank) {
-    		this.bank = bank;
-    	}
-    	public int toBitmask() {
-    		int bitmask = 0;
-    		bitmask |= ((suck ? 1 : 0) << 16);
-    		bitmask |= ((blow ? 1 : 0) << 17);
-    		bitmask |= ((light ? 1 : 0) << 18);
-    		return bitmask;
-    	}
+        public boolean suck = false;
+        public boolean blow = false;
+        public boolean light = false;
+        public int bank = 0;
+
+        public DigitalOutputs(int bank) {
+            this.bank = bank;
+        }
+
+        public int toBitmask() {
+            int bitmask = 0;
+            bitmask |= ((suck ? 1 : 0) << 16);
+            bitmask |= ((blow ? 1 : 0) << 17);
+            bitmask |= ((light ? 1 : 0) << 18);
+            return bitmask;
+        }
     }
 
     private DriverStatus getAxisStatus(int axis) throws Exception {
         String command = String.format("%1$d$", axis);
         String response = sendCommand(command);
         String data = response.split(Pattern.quote("$"))[1];
-        
+
         int bitmask = Integer.parseInt(data);
         DriverStatus status = new DriverStatus();
         status.ready = ((bitmask & (1 << 0)) != 0);
@@ -162,28 +163,28 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
 
         return status;
     }
-    
+
     private void initializeDigitalOutputs(DigitalOutputs outputs) throws Exception {
-    	String command = String.format("%1$dL255", outputs.bank);
+        String command = String.format("%1$dL255", outputs.bank);
         String response = sendCommand(command);
         Logger.trace(response);
         if (!response.equals(command)) {
             throw new Exception("Initialisierung der Digitalen Outputs gescheitert");
         }
     }
-    
+
     private void setDigitalOutputs(DigitalOutputs outputs) throws Exception {
-    	String command = String.format("%1$dY%2$d", outputs.bank, outputs.toBitmask());
+        String command = String.format("%1$dY%2$d", outputs.bank, outputs.toBitmask());
         String response = sendCommand(command);
         Logger.trace(response);
         if (!response.equals(command)) {
             throw new Exception("Digitalen Output setzen gescheitert");
         }
     }
-	
-	private void initializeAxis(int axis) throws Exception {
+
+    private void initializeAxis(int axis) throws Exception {
         // Positionsfehler zurücksetzen
-		String command = String.format("%1$dD", axis);
+        String command = String.format("%1$dD", axis);
         String response = sendCommand(command);
         Logger.trace(response);
         if (!response.equals(command)) {
@@ -201,43 +202,43 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
         if (!response.equals(command)) {
             throw new Exception("Laden der Daten aus dem EEPROM Fehlgeschlagen");
         }
-		
-		// Rampe einstellen
+
+        // Rampe einstellen
         command = String.format("%1$d:ramp_mode=0", axis);
-		response = sendCommand(command);
-		if (!response.equals(command)) {
-			throw new Exception("Setzen der Rampe fehlgeschlagen");
-		}
-		
-		// Motor starten
+        response = sendCommand(command);
+        if (!response.equals(command)) {
+            throw new Exception("Setzen der Rampe fehlgeschlagen");
+        }
+
+        // Motor starten
         startMotor(axis);
-		
+
         // wait for axis to hit the home switch
-		do {
-			Thread.sleep(10);
-			status = getAxisStatus(axis);
-		} while (!(status.ready && status.zeroPos));
-		
-		setAcceleration(axis);
-	}
-	
-	private void setAbsolutePositioning(int axis) throws Exception {
-		String command = String.format("%1$dp2", axis);
-		String response = sendCommand(command);
+        do {
+            Thread.sleep(10);
+            status = getAxisStatus(axis);
+        } while (!(status.ready && status.zeroPos));
+
+        setAcceleration(axis);
+    }
+
+    private void setAbsolutePositioning(int axis) throws Exception {
+        String command = String.format("%1$dp2", axis);
+        String response = sendCommand(command);
         if (!response.equals(command)) {
             throw new Exception("aktivieren von absoluter Positionierung gescheitert");
         }
-	}
-	
-	private void startMotor(int axis) throws Exception {
-		
-		String command = String.format("%1$dA", axis);
-		String response = sendCommand(command);
+    }
+
+    private void startMotor(int axis) throws Exception {
+
+        String command = String.format("%1$dA", axis);
+        String response = sendCommand(command);
         if (!response.equals(command)) {
             throw new Exception("Motor starten gescheitert");
         }
-	}
-	
+    }
+
     @Override
     public synchronized void connect() throws Exception {
         super.connect();
@@ -280,7 +281,7 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
                 throw new Exception("Schrittmodus falsch");
             }
         }
-        
+
         initializeDigitalOutputs(digitalOutputsBank1);
         initializeDigitalOutputs(digitalOutputsBank2);
     }
@@ -297,69 +298,71 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
         initializeAxis(Z_AXIS);
         initializeAxis(X_AXIS);
         initializeAxis(Y_AXIS);
-        
-		// Absolute Positionierung
-		setAbsolutePositioning(X_AXIS);
-		setAbsolutePositioning(Y_AXIS);
-		setAbsolutePositioning(Z_AXIS);
-		setAbsolutePositioning(C_AXIS);
-		
-		this.x = readAxisPosition(X_AXIS);
-		this.y = readAxisPosition(Y_AXIS);
-		this.z = readAxisPosition(Z_AXIS);
-		this.c = readAxisPosition(C_AXIS);
-    	
-    	digitalOutputsBank1.light = true;
-    	digitalOutputsBank2.light = true;
+
+        // Absolute Positionierung
+        setAbsolutePositioning(X_AXIS);
+        setAbsolutePositioning(Y_AXIS);
+        setAbsolutePositioning(Z_AXIS);
+        setAbsolutePositioning(C_AXIS);
+
+        this.x = readAxisPosition(X_AXIS);
+        this.y = readAxisPosition(Y_AXIS);
+        this.z = readAxisPosition(Z_AXIS);
+        this.c = readAxisPosition(C_AXIS);
+
+        digitalOutputsBank1.light = true;
+        digitalOutputsBank2.light = true;
         setDigitalOutputs(digitalOutputsBank1);
         setDigitalOutputs(digitalOutputsBank2);
     }
-    
-	private double readAxisPosition(int axis) throws Exception {
-		String command = String.format("%1$dC", axis);
-		String response = sendCommand(command);
-		String posString = response.split("C")[1];
-		Double pos = Integer.parseInt(posString.substring(1)) / stepsPerMm[axis];
-		return pos;
-	}
-    
+
+    private double readAxisPosition(int axis) throws Exception {
+        String command = String.format("%1$dC", axis);
+        String response = sendCommand(command);
+        String posString = response.split("C")[1];
+        Double pos = Integer.parseInt(posString.substring(1)) / stepsPerMm[axis];
+        return pos;
+    }
+
     private void moveAxisTo(int axis, double position) throws Exception {
-    	int steps = new Double(position * stepsPerMm[axis]).intValue();
-    	Logger.debug(String.format("Axis: %1d, Position: %2f, Steps: %3d", axis, position, steps));
-    	
-    	// Position setzen
-    	String command = String.format("%1$ds%2$d", axis, steps);
-		String response = sendCommand(command);
+        int steps = new Double(position * stepsPerMm[axis]).intValue();
+        Logger.debug(String.format("Axis: %1d, Position: %2f, Steps: %3d", axis, position, steps));
+
+        // Position setzen
+        String command = String.format("%1$ds%2$d", axis, steps);
+        String response = sendCommand(command);
         if (!response.equals(command)) {
             throw new Exception("Verfahren gescheitert");
         }
-        
+
         // Motor starten
         startMotor(axis);
     }
-    
+
     private void waitForMoveCompleted(int axis) throws Exception {
-    	// wait for axis to become ready again
+        // wait for axis to become ready again
         DriverStatus status = new DriverStatus();
-		do {
-			Thread.sleep(10);
-			status = getAxisStatus(axis);
-		} while (!status.ready);
+        do {
+            Thread.sleep(10);
+            status = getAxisStatus(axis);
+        } while (!status.ready);
     }
-    
+
     private void setFeedRate(int axis, double scale) throws Exception {
-    	int frequency = new Double(Math.abs(stepsPerMm[axis]) * (feedMmPerMinute[axis] / 60.0) * scale).intValue();
-    	String command = String.format("%1$do%2$d", axis, frequency);
+        int frequency = new Double(
+                Math.abs(stepsPerMm[axis]) * (feedMmPerMinute[axis] / 60.0) * scale).intValue();
+        String command = String.format("%1$do%2$d", axis, frequency);
         String response = sendCommand(command);
         Logger.trace(response);
         if (!response.equals(command)) {
             throw new Exception("Feedrate setzen gescheitert");
         }
     }
-    
+
     private void setAcceleration(int axis) throws Exception {
-    	int frequency_per_s = new Double(Math.abs(stepsPerMm[axis]) * (accelMmPerSecondSquared[axis] / 60.0)).intValue();
-    	String command = String.format("%1$d:accel%2$d", axis, frequency_per_s);
+        int frequency_per_s = new Double(
+                Math.abs(stepsPerMm[axis]) * (accelMmPerSecondSquared[axis] / 60.0)).intValue();
+        String command = String.format("%1$d:accel%2$d", axis, frequency_per_s);
         String response = sendCommand(command);
         Logger.trace(response);
         if (!response.equals(command)) {
@@ -370,12 +373,12 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
     @Override
     public void moveTo(ReferenceHeadMountable hm, Location location, double speed)
             throws Exception {
-    	
-    	// Verfahrweg einstellen: s<int>
-    	// Absolute Positionierung: p2
+
+        // Verfahrweg einstellen: s<int>
+        // Absolute Positionierung: p2
 
         location = location.convertToUnits(LengthUnit.Millimeters);
-    	location = location.subtract(hm.getHeadOffsets());
+        location = location.subtract(hm.getHeadOffsets());
 
         double x = location.getX();
         double y = location.getY();
@@ -389,59 +392,63 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
         boolean zValid = false;
         boolean cMoved = false;
         boolean cValid = false;
-        
+
         // correct C axis offset
         double xCorrection = (Math.sin(-c / 180.0 * Math.PI) * this.cAxisCorrectionRadius);
         double yCorrection = (Math.cos(-c / 180.0 * Math.PI) * this.cAxisCorrectionRadius);
         x += xCorrection;
         y += yCorrection;
-        
+
         if (!Double.isNaN(x) && ((x != this.x) || (xCorrection != this.xCorrection))) {
-        	if (checkLimit(X_AXIS, x)) {
-	        	xValid = true;
-        	} else {
-        		throw new Exception("Move would exceed limit on X axis!");
-        	}
+            if (checkLimit(X_AXIS, x)) {
+                xValid = true;
+            }
+            else {
+                throw new Exception("Move would exceed limit on X axis!");
+            }
         }
         if (!Double.isNaN(y) && ((y != this.y) || (yCorrection != this.yCorrection))) {
-        	if (checkLimit(Y_AXIS, y)) {
-	        	yValid = true;
-        	} else {
-        		throw new Exception("Move would exceed limit on Y axis!");
-        	}
+            if (checkLimit(Y_AXIS, y)) {
+                yValid = true;
+            }
+            else {
+                throw new Exception("Move would exceed limit on Y axis!");
+            }
         }
-        if (!Double.isNaN(z) && (z != this.z) && !hm.toString().contains("Camera")) {
-        	if (checkLimit(Z_AXIS, z)) {
-	        	zValid = true;
-        	} else {
-        		throw new Exception("Move would exceed limit on Z axis!");
-        	}
+        if (!Double.isNaN(z) && (z != this.z) && !hm.toString()
+                                                    .contains("Camera")) {
+            if (checkLimit(Z_AXIS, z)) {
+                zValid = true;
+            }
+            else {
+                throw new Exception("Move would exceed limit on Z axis!");
+            }
         }
         if (!Double.isNaN(c) && c != this.c) {
-        	cValid = true;
+            cValid = true;
         }
-        
+
         if (xValid) {
-        	setFeedRate(X_AXIS, speed);
+            setFeedRate(X_AXIS, speed);
             moveAxisTo(X_AXIS, x);
             xMoved = true;
         }
         if (yValid) {
-        	setFeedRate(Y_AXIS, speed);
+            setFeedRate(Y_AXIS, speed);
             moveAxisTo(Y_AXIS, y);
             yMoved = true;
         }
         if (zValid) {
-        	setFeedRate(Z_AXIS, speed);
+            setFeedRate(Z_AXIS, speed);
             moveAxisTo(Z_AXIS, z);
             zMoved = true;
         }
         if (cValid) {
-        	setFeedRate(C_AXIS, speed);
+            setFeedRate(C_AXIS, speed);
             moveAxisTo(C_AXIS, c);
             cMoved = true;
         }
-        
+
         if (xMoved) {
             waitForMoveCompleted(X_AXIS);
             this.x = readAxisPosition(X_AXIS) - xCorrection;
@@ -458,7 +465,8 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
         }
         if (cMoved) {
             waitForMoveCompleted(C_AXIS);
-            this.c = readAxisPosition(C_AXIS) - hm.getHeadOffsets().getRotation();
+            this.c = readAxisPosition(C_AXIS) - hm.getHeadOffsets()
+                                                  .getRotation();
         }
     }
 
@@ -471,7 +479,7 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
 
     @Override
     public void place(ReferenceNozzle nozzle) throws Exception {
-    	digitalOutputsBank2.suck = false;
+        digitalOutputsBank2.suck = false;
         digitalOutputsBank2.blow = true;
         setDigitalOutputs(digitalOutputsBank2);
         Thread.sleep(this.placeBlowTime);
@@ -486,7 +494,7 @@ public class DanielPnpDriver extends AbstractSerialPortDriver implements Runnabl
 
     @Override
     public void actuate(ReferenceActuator actuator, boolean on) throws Exception {
-    	// unused: auto generated function stub
+        // unused: auto generated function stub
     }
 
     @Override
